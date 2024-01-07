06df62601d0a43c7f1c2d062e7b0b327c4c0eb83
aaaa <a@a>
2024-01-07 17:44:21 +0900

    Initial Commit

---
+++ SpatialChess/SpatialChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialChessApp.swift
+//  SpatialChess
+//
+//  Created by FULLNAME on 2024/01/07.
+//
+
+import SwiftUI
+
+@main
+struct SpatialChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
