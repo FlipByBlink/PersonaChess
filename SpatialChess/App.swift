adb6cc84691fac7384a9655ede8a4fcdf3dd5d2a
aaaa <a@a>
2024-01-30 10:44:17 +0900

    a

--- SpatialChess/App.swift
+++ SpatialChess/App.swift
@@ -3,17 +3,8 @@ import SwiftUI
 @main
 struct SpatialChessApp: App {
     var body: some Scene {
-        WindowGroup {
+        ImmersiveSpace {
             ContentView()
-                .border(.pink, width: 3)
-                .overlay {
-                    Circle()
-                        .frame(width: 10, height: 10)
-                }
         }
-        .windowStyle(.volumetric)
-        .defaultSize(width: 1400,
-                     height: 500,
-                     depth: 1400)
     }
 }
--- SpatialChess/ContentView.swift
+++ SpatialChess/ContentView.swift
@@ -2,13 +2,17 @@ import SwiftUI
 import RealityKit
 
 struct ContentView: View {
+    @State private var rootEntity: Entity = .init()
     var body: some View {
         RealityView { content in
+            self.rootEntity.position.y = 1.2
+            self.rootEntity.position.z = -1
             GameState.preset.forEach { (key: GameState.Position, value: Piece) in
                 let entity = try! Entity.load(named: value.assetName)
                 entity.position = key.position
-                content.add(entity)
+                self.rootEntity.addChild(entity)
             }
+            content.add(self.rootEntity)
         }
     }
 }
