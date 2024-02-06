a71a2e22a7b43c70689580de4e80dcdb759af3e6
aaaa <a@a>
2024-02-06 15:23:35 +0900

    a

--- SpatiallyChess/ContentView.swift
+++ SpatiallyChess/ContentView.swift
@@ -4,26 +4,8 @@ import RealityKit
 struct ContentView: View {
     @StateObject var model: AppModel = .init()
     var body: some View {
-        RealityView { content, attachments in
-            self.model.setUpEntities()
-            self.model.rootEntity.addChild(attachments.entity(for: "board")!)
-            self.model.rootEntity.addChild(attachments.entity(for: "toolbar")!)
-            content.add(self.model.rootEntity)
-        } attachments: {
-            Attachment(id: "board") {
-                BoardView()
-                    .environmentObject(self.model)
-            }
-            Attachment(id: "toolbar") {
-                ToolbarView()
-                    .environmentObject(self.model)
-            }
-        }
-        .gesture(
-            TapGesture()
-                .targetedToEntity(where: .has(PieceStateComponent.self))
-                .onEnded { self.model.executeAction(.tapPiece($0.entity)) }
-        )
-        .task { 📢SoundEffect.setCategory() }
+        🌐RealityView()
+            .task { 📢SoundEffect.setCategory() }
+            .environmentObject(self.model)
     }
 }
---
+++ SpatiallyChess/🌐RealityView.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+import RealityKit
+
+struct 🌐RealityView: View {
+    @EnvironmentObject var model: AppModel
+    var body: some View {
+        RealityView { content, attachments in
+            self.model.setUpEntities()
+            self.model.rootEntity.addChild(attachments.entity(for: "board")!)
+            self.model.rootEntity.addChild(attachments.entity(for: "toolbar")!)
+            content.add(self.model.rootEntity)
+        } attachments: {
+            Attachment(id: "board") {
+                BoardView()
+                    .environmentObject(self.model)
+            }
+            Attachment(id: "toolbar") {
+                ToolbarView()
+                    .environmentObject(self.model)
+            }
+        }
+        .gesture(
+            TapGesture()
+                .targetedToEntity(where: .has(PieceStateComponent.self))
+                .onEnded { self.model.executeAction(.tapPiece($0.entity)) }
+        )
+    }
+}
