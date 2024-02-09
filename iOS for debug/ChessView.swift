41046ca20af08b3719884130bb03eadea27cabef
aaaa <a@a>
2024-02-09 17:37:02 +0900

    a

--- iOS for debug/ChessView.swift
+++ iOS for debug/ChessView.swift
@@ -117,7 +117,7 @@ extension Piece {
                 }
             case .black:
                 switch self.chessmen {
-                    case .pawn0, .pawn1, .pawn2, .pawn3, .pawn4, .pawn5, .pawn6, .pawn7: "♟"
+                    case .pawn0, .pawn1, .pawn2, .pawn3, .pawn4, .pawn5, .pawn6, .pawn7: "♟\u{FE0E}"
                     case .rook0, .rook1: "♜"
                     case .knight0, .knight1: "♞"
                     case .bishop0, .bishop1: "♝"
--- iOS for debug/SharePlayMenu.swift
+++ iOS for debug/SharePlayMenu.swift
@@ -6,20 +6,24 @@ struct SharePlayMenu: View {
     @StateObject private var groupStateObserver = GroupStateObserver()
     var body: some View {
         VStack {
-            Text("eligibleForGroupSession: \(self.groupStateObserver.isEligibleForGroupSession.description)")
+            Text("groupSession?.state: " + String(describing: self.model.groupSession?.state))
                 .font(.caption)
-            Button("Start activity!") { self.model.activateGroupActivity() }
-                .disabled(!self.groupStateObserver.isEligibleForGroupSession)
-            Button("Restart") { self.model.restartGroupActivity() }
-                .disabled(!self.groupStateObserver.isEligibleForGroupSession)
-        }
-        .padding()
-        .opacity({
-            switch self.model.groupSession?.state {
-                case .joined: 0
-                case .waiting: 0.4
-                default: 1
+            VStack {
+                Text("eligibleForGroupSession: \(self.groupStateObserver.isEligibleForGroupSession.description)")
+                    .font(.caption)
+                Button("Start activity!") { self.model.activateGroupActivity() }
+                    .disabled(!self.groupStateObserver.isEligibleForGroupSession)
+                Button("Restart") { self.model.restartGroupActivity() }
+                    .disabled(!self.groupStateObserver.isEligibleForGroupSession)
             }
-        }())
+            .padding()
+            .opacity({
+                switch self.model.groupSession?.state {
+                    case .joined: 0
+                    case .waiting: 0.4
+                    default: 1
+                }
+            }())
+        }
     }
 }
