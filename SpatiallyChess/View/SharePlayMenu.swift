979a57fe2c9ee92a7b985a3095fceb03d39a7a89
aaaa <a@a>
2024-02-11 15:44:27 +0900

    a

--- SpatiallyChess/View/SharePlayMenu.swift
+++ SpatiallyChess/View/SharePlayMenu.swift
@@ -6,20 +6,32 @@ struct SharePlayMenu: View {
     @StateObject private var groupStateObserver = GroupStateObserver()
     var body: some View {
         NavigationStack {
-            VStack {
+            VStack(spacing: 16) {
                 Text("eligibleForGroupSession: \(self.groupStateObserver.isEligibleForGroupSession.description)")
-                    .font(.caption)
+                Text({
+                    "groupSession?.state: "
+                    +
+                    {
+                        switch self.model.groupSession?.state {
+                            case .waiting: "waiting"
+                            case .joined: "joined"
+                            case .invalidated(reason: let error): "invalidated(\(error))"
+                            case .none: "none"
+                            @unknown default: "@unknown default"
+                        }
+                    }()
+                }())
                 Button("Start activity!") { self.model.activateGroupActivity() }
                     .disabled(!self.groupStateObserver.isEligibleForGroupSession)
                 Button("Restart") { self.model.restartGroupActivity() }
                     .disabled(!self.groupStateObserver.isEligibleForGroupSession)
             }
-            .navigationTitle("SharePlay chess")
+            .navigationTitle("SpatiallyChess")
             .padding(32)
         }
         .glassBackgroundEffect()
         .opacity(self.model.groupSession?.state == .joined ? 0 : 1)
-        .frame(width: 500, height: 400)
+        .frame(width: 800, height: 500)
         .padding(.bottom, 300)
     }
 }
