f00b02d43a29426409def4b41c70f95fb3c6ea8a
aaaa <a@a>
2024-02-11 16:03:21 +0900

    a

--- SpatiallyChess/View/SharePlayMenu.swift
+++ SpatiallyChess/View/SharePlayMenu.swift
@@ -6,25 +6,21 @@ struct SharePlayMenu: View {
     @StateObject private var groupStateObserver = GroupStateObserver()
     var body: some View {
         NavigationStack {
-            VStack(spacing: 16) {
-                Text("eligibleForGroupSession: \(self.groupStateObserver.isEligibleForGroupSession.description)")
-                Text({
-                    "groupSession?.state: "
-                    +
-                    {
-                        switch self.model.groupSession?.state {
-                            case .waiting: "waiting"
-                            case .joined: "joined"
-                            case .invalidated(reason: let error): "invalidated(\(error))"
-                            case .none: "none"
-                            @unknown default: "@unknown default"
-                        }
-                    }()
-                }())
+            List {
+                Section {
+                    LabeledContent {
+                        Text("\(self.groupStateObserver.isEligibleForGroupSession.description)")
+                    } label: {
+                        Text("Eligible for SharePlay:")
+                    }
+                } footer: {
+                    Text("SharePlay can be used during a FaceTime call.")
+                }
                 Button("Start activity!") { self.model.activateGroupActivity() }
                     .disabled(!self.groupStateObserver.isEligibleForGroupSession)
-                Button("Restart") { self.model.restartGroupActivity() }
-                    .disabled(!self.groupStateObserver.isEligibleForGroupSession)
+                Section {
+                    self.groupSessionStateText()
+                }
             }
             .navigationTitle("SpatiallyChess")
             .padding(32)
@@ -33,5 +29,27 @@ struct SharePlayMenu: View {
         .opacity(self.model.groupSession?.state == .joined ? 0 : 1)
         .frame(width: 800, height: 500)
         .padding(.bottom, 300)
+    }
+}
+
+private extension SharePlayMenu {
+    private func groupSessionStateText() -> some View {
+        LabeledContent {
+            Text({
+                switch self.model.groupSession?.state {
+                    case .waiting: "waiting"
+                    case .joined: "joined"
+                    case .invalidated(reason: let error): "invalidated(\(error.localizedDescription))"
+                    case .none: "none"
+                    @unknown default: "@unknown default"
+                }
+            }())
+        } label: {
+            Text("groupSession?.state:")
+        }
+    }
+    private func restartButton() -> some View {
+        Button("Restart") { self.model.restartGroupActivity() }
+            .disabled(!self.groupStateObserver.isEligibleForGroupSession)
     }
 }
