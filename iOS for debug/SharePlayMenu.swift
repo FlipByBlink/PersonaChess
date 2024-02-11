013df8d4ba495c47d8a62b99c0d57bba00a3fdaf
aaaa <a@a>
2024-02-11 14:22:11 +0900

    a

--- iOS for debug/SharePlayMenu.swift
+++ iOS for debug/SharePlayMenu.swift
@@ -20,22 +20,20 @@ struct SharePlayMenu: View {
                 }()
             }())
             .font(.caption)
-            VStack {
-                Text("eligibleForGroupSession: \(self.groupStateObserver.isEligibleForGroupSession.description)")
-                    .font(.caption)
-                Button("Start activity!") { self.model.activateGroupActivity() }
-                    .disabled(!self.groupStateObserver.isEligibleForGroupSession)
-                Button("Restart") { self.model.restartGroupActivity() }
-                    .disabled(!self.groupStateObserver.isEligibleForGroupSession)
+            Text("eligibleForGroupSession: \(self.groupStateObserver.isEligibleForGroupSession.description)")
+                .font(.caption)
+            Button("Start activity!") {
+                self.model.activateGroupActivity()
             }
-            .opacity({
-                switch self.model.groupSession?.state {
-                    case .joined: 0
-                    case .waiting: 0.5
-                    case .invalidated(reason: _): 0.1
-                    default: 1
-                }
-            }())
+            .disabled(
+                !self.groupStateObserver.isEligibleForGroupSession
+                ||
+                self.model.groupSession?.state != nil
+            )
+            Button("Restart") {
+                self.model.restartGroupActivity()
+            }
+            .disabled(!self.groupStateObserver.isEligibleForGroupSession)
         }
         .padding()
     }
