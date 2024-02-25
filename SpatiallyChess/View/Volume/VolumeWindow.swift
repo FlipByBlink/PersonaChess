6ccb79c79718ff3514e0f55ddbbd3c89207bc316
aaaa <a@a>
2024-02-25 17:37:42 +0900

    a

--- SpatiallyChess/View/Volume/VolumeWindow.swift
+++ SpatiallyChess/View/Volume/VolumeWindow.swift
@@ -5,15 +5,21 @@ struct VolumeWindow: Scene {
     var body: some Scene {
         WindowGroup(id: "volume") {
             VolumeView()
-                .environmentObject(self.model)
+                .environmentObject(self.model) //※1
         }
-        .defaultSize(width: Size.Meter.board,
-                     height: Size.Meter.board,
-                     depth: Size.Meter.board,
-                     in: .meters)
+        .defaultSize(width: Size.Meter.board + 0.25,
+                     height: Size.Meter.board + 0.25,
+                     depth: Size.Meter.board + 0.25,
+                     in: .meters) //※2
         .windowResizability(.contentSize)
         .windowStyle(.volumetric)
     }
 }
 
-//Workaround: FullSpaceから改めてWindowを開いた際にEnvironmentObjectが機能しない場合があるので二重にenvironmentObject
+//==== ※1 workaround ====
+//不具合: FullSpaceから改めてWindowを開いた際にEnvironmentObjectが機能しない場合がある
+//対応: 冗長になるがここでもenvironmentObject(self.model)
+//==== ※2 workaround ====
+//不具合: visionOS1.0のバグでWindowZoomがvolumeに対して適切に動作しない不具合に対応するため少し大きめ(0.25m)に調整
+//対応: volumeのサイズを元々少し大きめ(0.25m)に調整
+//=======================
