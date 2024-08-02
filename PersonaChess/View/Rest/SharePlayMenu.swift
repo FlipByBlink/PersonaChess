c15369dcedc96a8086a3e6b82b09341188fde89f
aaaa <a@a>
2024-08-03 08:58:49 +0900

    wip

--- PersonaChess/View/Rest/SharePlayMenu.swift
+++ PersonaChess/View/Rest/SharePlayMenu.swift
@@ -137,24 +137,34 @@ private extension SharePlayMenu {
     private func aboutOptionsMenu() -> some View {
         List {
             Section {
-                Text(verbatim: "Open toolbar bottom a board.")
+                Label("Open the toolbar at the bottom of a board.",
+                      systemImage: "ellipsis")
             }
             Section {
-                Text("You can change the board’s size and height, rotation.")
-                    .padding()
+                Label("Adjust the rotation of the board.",
+                      systemImage: "arrow.turn.right.up")
+                Label("Adjust the size of the board.",
+                      systemImage: "plusminus")
+                Label("Adjust the height of the board.",
+                      systemImage: "chevron.up.chevron.down")
+            }
+            Section {
+                Label("The Undo feature allows you to take back your last move.",
+                      systemImage: "arrow.uturn.backward")
+                Label("The Reset feature allows you to restart the chess game from the beginning.",
+                      systemImage: "arrow.counterclockwise")
+            }
+            Section {
                 HStack(spacing: 24) {
+                    Label("By setting the board’s height equal to the floor, the board will seamlessly integrate with the floor.",
+                          systemImage: "arrow.down.to.line")
                     Image(.floorModeExample)
                         .resizable()
                         .scaledToFit()
                         .frame(width: 360)
                         .clipShape(.rect(cornerRadius: 6))
-                    Text("By setting the board’s height equal to the floor, the board will seamlessly integrate with the floor.")
                 }
                 .padding()
-            }
-            Section {
-                Text(verbatim: "Undo function")
-                Text(verbatim: "Reset function")
             }
         }
         .navigationTitle("About options")
