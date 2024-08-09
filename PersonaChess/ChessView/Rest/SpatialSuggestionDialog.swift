4408f08791ec11c1e32c4f4ae9e0c479f0efd926
aaaa <a@a>
2024-08-09 09:12:38 +0900

    a

--- PersonaChess/ChessView/Rest/SpatialSuggestionDialog.swift
+++ PersonaChess/ChessView/Rest/SpatialSuggestionDialog.swift
@@ -3,26 +3,41 @@ import SwiftUI
 struct SpatialSuggestionDialog: View {
     @EnvironmentObject var model: AppModel
     @Environment(\.physicalMetrics) var physicalMetrics
+    @State private var dismissedManually: Bool = false
     var body: some View {
-        Label {
-            Text("""
+        HStack {
+            Label {
+                Text("""
                 You are currently not in Spatial mode.
                 Please switch to Spatial mode from Control Center or FaceTime window.
                 """)
-        } icon: {
-            Image(systemName: "exclamationmark.triangle.fill")
-                .imageScale(.large)
-                .padding(8)
+            } icon: {
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .imageScale(.large)
+                    .padding(8)
+                    .symbolRenderingMode(.multicolor)
+            }
+            .fixedSize()
+            .padding()
+            .padding(.horizontal, 8)
+            .glassBackgroundEffect()
+            Button {
+                self.dismissedManually = true
+            } label: {
+                Image(systemName: "xmark")
+                    .padding(10)
+            }
+            .foregroundStyle(.secondary)
+            .buttonStyle(.plain)
+            .buttonBorderShape(.circle)
+            .glassBackgroundEffect()
         }
-        .fixedSize()
-        .symbolRenderingMode(.multicolor)
-        .padding()
-        .padding(.horizontal, 8)
-        .glassBackgroundEffect()
         .opacity(self.isPresented ? 1.0 : 0)
+        .opacity(self.dismissedManually ? 0 : 1.0)
         .animation(.default, value: self.isPresented)
-        .offset(y: -1800)
-        .offset(z: -1220 - Size.Point.board(self.physicalMetrics))
+        .animation(.default, value: self.dismissedManually)
+        .offset(y: -1400)
+        .offset(z: -1200 - (Size.Point.board(self.physicalMetrics) / 2))
     }
 }
 
