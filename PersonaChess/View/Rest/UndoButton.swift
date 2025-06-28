415a5fc47af57aed25d9128bba21ea055cac9bd2
a <a@a>
2025-06-28 12:35:50 +0900

    remove ToolbarViewOnHand

--- PersonaChess/View/Rest/UndoButton.swift
+++ PersonaChess/View/Rest/UndoButton.swift
@@ -3,27 +3,13 @@ import SwiftUI
 struct UndoButton: View {
     @EnvironmentObject var model: AppModel
     
-    var kind: Self.Kind = .toolbar
-    
     var body: some View {
         Button {
             self.model.execute(.undo)
         } label: {
-            switch self.kind {
-                case .toolbar:
-                    Label("Undo", systemImage: "arrow.uturn.backward")
-                case .hand:
-                    Label("Undo", systemImage: "arrow.uturn.backward")
-                        .labelStyle(.iconOnly)
-                        .frame(width: ToolbarViewOnHand.ContentView.circleButtonSize,
-                               height: ToolbarViewOnHand.ContentView.circleButtonSize)
-            }
+            Label("Undo", systemImage: "arrow.uturn.backward")
         }
         .disabled(self.model.sharedState.logs.isEmpty)
         .disabled(self.model.isAnimating)
-    }
-    
-    enum Kind {
-        case toolbar, hand
     }
 }
